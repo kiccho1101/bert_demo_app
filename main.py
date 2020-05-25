b32e19a65cca405a45a0dcf0688361a2299c8813
yodai.a.kishimoto <yodai.a.kishimoto@rakuten.com>
2020-05-25 18:00:24 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# %%
+import torch
+import string
+
+from transformers import BertTokenizer, BertForMaskedLM
+
+bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+bert_model = BertForMaskedLM.from_pretrained("bert-base-uncased").eval()
+
+
+top_k = 10
+
+
+def decode(tokenizer, pred_idx, top_clean):
+    ignore_tokens = string.punctuation + "[PAD]"
+    tokens = []
+    for w in pred_idx:
+        token = "".join(tokenizer.decode(w).split())
+        if token not in ignore_tokens:
+            tokens.append(token.replace("##", ""))
+    return "\n".join(tokens[:top_clean])
+
+
+def encode(tokenizer, text_sentence, add_special_tokens=True):
+    text_sentence = text_sentence.replace("<mask>", tokenizer.mask_token)
+    # if <mask> is the last token, append a "." so that models dont predict punctuation.
+    if tokenizer.mask_token == text_sentence.split()[-1]:
+        text_sentence += " ."
+
+    input_ids = torch.tensor(
+        [tokenizer.encode(text_sentence, add_special_tokens=add_special_tokens)]
+    )
+    mask_idx = torch.where(input_ids == tokenizer.mask_token_id)[1].tolist()[0]
+    return input_ids, mask_idx
+
+
+def get_all_predictions(text_sentence, top_clean=5):
+    # ========================= BERT =================================
+    print(text_sentence)
+    input_ids, mask_idx = encode(bert_tokenizer, text_sentence)
+    with torch.no_grad():
+        predict = bert_model(input_ids)[0]
+    bert = decode(
+        bert_tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean
+    )
+
+    return {"bert": bert}
